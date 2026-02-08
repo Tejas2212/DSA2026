837c0958aece979692f5a49584afa8fb3c084b43
Tejas <tejasbhagat54@gmail.com>
2026-02-08 12:50:59 +0530

    dfsiterative solution

--- Graph/BFS/bfs.java
+++ Graph/BFS/bfs.java
@@ -4,22 +4,23 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Queue;
 
-public class bfs {
-        public static void bfsTraversal(int src,  ArrayList<ArrayList<Integer>> graph, boolean []vis){
-        Queue<Integer> q= new ArrayDeque<>();
-        q.add(src);
-
-        while(!q.isEmpty()){
-            int a= q.remove();
-            if(vis[a]){
-                continue;
-            }
-            vis[a]=true;
-            System.out.println(a+" ");
-            for(Integer temp : graph.get(a)){
-                
-                q.add(temp);
-            }
-        }
+class Solution {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> a) {
+       Queue<Integer> q= new ArrayDeque<>();
+	   ArrayList<Integer> ans = new ArrayList<>();
+	   q.add(0);
+	   boolean [] vis = new boolean[a.size()];
+	   while(q.size()!=0){
+	     int temp =q.remove();
+		 if(vis[temp])continue;
+		 vis[temp]=true;
+		 ans.add(temp);
+		 for(Integer i: a.get(temp)){
+		   if(vis[i]==false){
+		    q.add(i);
+		   }
+		  }
+	   }
+	   return ans;
     }
 }
